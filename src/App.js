2da86d22644b2d478db2480a472170d56cb8fe77
FilipeMMendes <mendes.m.filipe@gmail.com>
2021-09-26 02:33:21 -0300

    feat: configuration and style of the chat engine

--- src/App.js
+++ src/App.js
@@ -1,25 +1,16 @@
-import logo from './logo.svg';
+import { ChatEngine } from 'react-chat-engine';
+
 import './App.css';
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <ChatEngine
+      height='100vh'
+      userName='chat'
+      userSecret='123456'
+      projectID='481c5847-aefa-4a79-90b5-466d109bf08f'
+    />
   );
-}
+};
 
 export default App;
